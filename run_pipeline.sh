e9d5a32fb9adda1b287e4e8de0768d1de86a6caf
Felix Hofmann <felix.hofmann2@uzh.ch>
2022-01-14 12:39:33 +0100

    edit simulation running script

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 
-cd simulation
+FILE=simulation/error.txt
 
-make
-
-cd ..
-
-git add simulation/RData/* simulation/simulate_all.Rout
-
-git commit -m "Push simulation results"
-
-git push
-
+if [ -f "$FILE" ]; then
+  printf "$FILE exists from a previous run.\nDo you want to delete it and continue? y/n\n"
+  while [ true ];
+  do 
+    read -p "Input Selection: " INPUT
+    if [ "$INPUT" = "y" ]; then
+      printf "Removing $FILE ...\n"
+      rm -f "$FILE"
+      printf "Start running the simulation ...\n"
+      cd simulation && 
+      make && 
+      cd .. &&
+      if [ ! -f "$FILE" ]; then
+        git add RData/* simulate_all.Rout &&
+        git commit -m "Push simulation results" &&
+        git push
+      fi
+      break
+    elif [ "$INPUT" = "n" ]; then
+      printf "Aborting pipeline.\n"
+      break
+    else
+      printf "Invalid selection. Input must be either 'y' or 'n'.\n"
+    fi
+  done
+else 
+  printf "Start running the simulation ...\n"
+  cd simulation && 
+  make && 
+  cd .. &&
+  if [ ! -f "$FILE" ]; then
+    git add RData/* simulate_all.Rout &&
+    git commit -m "Push simulation results" &&
+    git push
+  fi
+fi
