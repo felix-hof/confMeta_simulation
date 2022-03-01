6939c524e9dce1e82c13be80f8fe53484d5c1536
Felix Hofmann <felix_hofmann_13@hotmail.com>
2022-03-01 13:05:44 +0100

    Edit plotting script

--- simulation/simulate_all_plot_results.R
+++ simulation/simulate_all_plot_results.R
@@ -15,7 +15,11 @@ make_title <- function(df){
     if(any(grepl("Effect", nms))){
         nms[grepl("Effect", nms)] <- ifelse(grep("Effect", nms) == 1, "theta~\"", "\"~theta~\"")
     }
+    if(any(grepl("I2", nms))){
+        nms[grepl("I2", nms)] <- ifelse(grep("I2", nms) == 1, "I^2~\"", "\"~I^2~\"")
+    }
     if(any(nms == "Large")){nms[nms == "Large"] <- "No. of large studies"}
+    if(any(nms == "K")){nms[nms == "K"] <- "No. of studies"}
     if(any(nms == "Heterogeneity")){nms[nms == "Heterogeneity"] <- "Simulation model"}
     vals <- unname(unlist(df[1, ]))
     args <- ifelse(grepl("theta", nms[1]), 
@@ -148,7 +152,7 @@ for(x in list_seq){ # loop over summary (eg. dist)
     current_levels <- opts[[current]]
     cat("Currently constructing plots for:", current, paste0("(", paste0(current_levels, collapse = ", "), ")"), "\n")
     grid_others <- expand.grid(opts[list_seq[list_seq != x]], stringsAsFactors = FALSE)
-    for(y in seq_len(nrow(grid_others))){ # loop over all combinations of other parameters (bias, large, he)
+    for(y in seq_len(nrow(grid_others))){ # loop over all combinations of other parameters (e.g. bias, large, he)
         # filter the data by current parameter combination
         filters <- lapply(seq_along(grid_others), function(z){
             lhs <- names(grid_others)[z]
@@ -205,129 +209,151 @@ for(x in list_seq){ # loop over summary (eg. dist)
 
 dir.create("figs/min_pH", showWarnings = FALSE, recursive = TRUE)
 
-out_gammamin %>% select(dist) %>% unique() %>% pull() -> dist
-out_gammamin %>% select(bias) %>% unique() %>% pull() -> bias
-out_gammamin %>% select(large) %>% unique() %>% pull() -> large
-out_gammamin %>% select(heterogeneity) %>% unique() %>% pull() -> heterogeneity
-out_gammamin %>% select(I2) %>% unique() %>% pull() -> I2
-#out_gammamin %>% select(measure) %>% unique() %>% pull() -> me
+opts <- list(dist = out_gammamin %>% pull(dist) %>% unique(),
+             bias = out_gammamin %>% pull(bias) %>% unique(),
+             large = out_gammamin %>% pull(large) %>% unique(),
+             heterogeneity = out_gammamin %>% pull(heterogeneity) %>% unique(),
+             I2 = out_gammamin %>% pull(I2) %>% unique())
+measure_opts <- out_gammamin %>% pull(measure) %>% unique()
 
-for(di in dist){
-    for(la in large){
-        for(bi in bias){
-            for(he in heterogeneity){
-                for(i in I2){
-                    pars <- out_gammamin %>%
-                        filter(dist == di, bias == bi, large == la, heterogeneity == he, I2 == i) %>%
-                        ggplot(aes(x = k, y = value, colour = measure)) +
-                        facet_wrap(~ method) +
-                        geom_point() +
-                        geom_line() +
-                        xlab("# studies") +
-                        labs(colour = "Statistic") +
-                        ggtitle(bquote("dist:"~.(di)~", bias: "~.(bi)~", "~theta~": 0.2, "~I^2~": "~.(i)~", no. of large studies: "~.(la)~", simulation model: "~.(he)))
-                    ggsave(filename = paste0("figs/min_pH/MIN-PH_", di, "_bias_", bi, "_large_", la,"_sim-mod_", he, "_I2_", i, ".png"),
-                           width = 10,
-                           height = 11,
-                           units = "in")
+list_seq <- seq_along(opts)
 
-
-                }
-            }
-        }
+for(x in list_seq){ # loop over summary (eg. dist)
+    current <- names(opts)[x]
+    current_levels <- opts[[current]]
+    cat("Currently constructing plots for:", current, paste0("(", paste0(current_levels, collapse = ", "), ")"), "\n")
+    grid_others <- expand.grid(opts[list_seq[list_seq != x]], stringsAsFactors = FALSE)
+    for(y in seq_len(nrow(grid_others))){ # loop over all combinations of other parameters (e.g. bias, large, he)
+        # filter the data by current parameter combination
+        filters <- lapply(seq_along(grid_others), function(z){
+            lhs <- names(grid_others)[z]
+            op <- quote(`==`)
+            rhs <- grid_others[y, z]
+            expr(`!!`(op)(!!sym(lhs), !!rhs))
+        })
+        img_data <- out_gammamin %>% 
+            filter(!!!filters) %>% 
+            mutate(method = factor(method, 
+                                   levels = c("Harmonic Mean CI (chisq)", "Harmonic Mean Additive CI (chisq)",
+                                              "Harmonic Mean Multiplicative CI (chisq)", "Harmonic Mean CI (f)", 
+                                              "Harmonic Mean Additive CI (f)", "Harmonic Mean Multiplicative CI (f)")))
+        lapply(current_levels, function(z){
+            title <- img_data %>% 
+                filter(!!sym(current) == z) %>% 
+                select(-k, -method, -measure, -value, -all_of(current), -sampleSize) %>%
+                distinct() %>%
+                bind_cols(!!current := z) %>%
+                select(order(colnames(.))) %>%
+                make_title()
+            img_data %>% 
+                filter(!!sym(current) == z) %>% 
+                ggplot(aes(x = k, y = value, color = measure)) +
+                geom_point() +
+                geom_line() +
+                ylim(c(0, 1)) +
+                facet_wrap(~method) +
+                labs(x = "# studies", y = "value",
+                     color = "Summary statistic",
+                     title = eval(title))
+        }) %>% 
+            wrap_plots(guides = "collect") &
+            theme(legend.position = "bottom",
+                  text = element_text(size = 9),
+                  plot.title = element_text(size = 10))
+        ggsave(filename = paste0("figs/min_pH/", toupper(current), "_", 
+                                 paste0(paste0(names(grid_others[y, ]), "_", grid_others[y, ]), collapse = "_"),
+                                 ".png"),
+               width = length(current_levels) * 7,
+               height = 12,
+               units = "in",
+               type = "cairo")
     }
 }
 
 
 ## relative frequencies -----------------------------------------------------------
 
+# Adapt make_title function for now
+make_title <- function(df){
+    nms <- names(df)
+    nms <- vapply(strsplit(nms, ""), 
+                  function(x){x[1] <- toupper(x[1]); paste0(x, collapse = "")},
+                  character(1L))
+    if(any(grepl("Effect", nms))){
+        nms[grepl("Effect", nms)] <- ifelse(grep("Effect", nms) == 1, "theta~\"", "\"~theta~\"")
+    }
+    if(any(grepl("I2", nms))){
+        nms[grepl("I2", nms)] <- ifelse(grep("I2", nms) == 1, "I^2~\"", "\"~I^2~\"")
+    }
+    if(any(nms == "Large")){nms[nms == "Large"] <- "# large studies"}
+    if(any(nms == "K")){nms[nms == "K"] <- "# studies"}
+    if(any(nms == "Heterogeneity")){nms[nms == "Heterogeneity"] <- "Simulation model"}
+    vals <- unname(unlist(df[1, ]))
+    args <- ifelse(grepl("theta", nms[1]), 
+                   paste0("bquote(", paste0(paste0(nms, ": ", vals), collapse = ", "), "\")"),
+                   paste0("bquote(\"", paste0(paste0(nms, ": ", vals), collapse = ", "), "\")"))
+    return(eval(parse(text = args)))
+}
+
 dir.create("figs/rel_freq", showWarnings = FALSE, recursive = TRUE)
 
-out_n %>% select(dist) %>% unique() %>% pull() -> dist
-out_n %>% select(bias) %>% unique() %>% pull() -> bias
-out_n %>% select(large) %>% unique() %>% pull() -> large
-out_n %>% select(heterogeneity) %>% unique() %>% pull() -> heterogeneity
-out_n %>% select(I2) %>% unique() %>% pull() -> I2
-out_n %>% select(k) %>% unique() %>% pull() -> k
+opts <- list(dist = out_n %>% pull(dist) %>% unique(),
+             bias = out_n %>% pull(bias) %>% unique(),
+             large = out_n %>% pull(large) %>% unique(),
+             heterogeneity = out_n %>% pull(heterogeneity) %>% unique(),
+             I2 = out_n %>% pull(I2) %>% unique(),
+             k = out_n %>% pull(k) %>% unique())
+measure_opts <- out_n %>% pull(measure) %>% unique()
 
-for(di in dist){
-    for(la in large){
-        for(bi in bias){
-            for(he in heterogeneity){
-                for(i in I2){
-                    for(K in k){
-                        out_n %>%
-                            filter(dist == di, bias == bi, large == la, heterogeneity == he, I2 == i, k == K) %>%
-                            ggplot(aes(x = measure, y = value, fill = k)) +
-                            facet_wrap(~ method) +
-                            geom_col() +
-                            xlab("# intervals") +
-                            ylab("relative frequency") +
-                            theme(legend.position = "none") +
-                            ggtitle(bquote("# studies: "~.(K)~", dist:"~.(di)~", bias: "~.(bi)~", "~theta~": 0.2, "~I^2~": "~.(i)~", # large studies: "~.(la)~", simulation model: "~.(he)))
-                        ggsave(filename = paste0("figs/rel_freq/k_", K, "_", di, "_bias_", bi, "_large_", la,"_sim-mod_", he, "_I2_", i, ".png"),
-                               width = 10,
-                               height = 11,
-                               units = "in")
+list_seq <- seq_along(opts)
 
-
-                    }
-                }
-            }
-        }
+for(x in list_seq){ # loop over summary (eg. dist)
+    current <- names(opts)[x]
+    current_levels <- opts[[current]]
+    cat("Currently constructing plots for:", current, paste0("(", paste0(current_levels, collapse = ", "), ")"), "\n")
+    grid_others <- expand.grid(opts[list_seq[list_seq != x]], stringsAsFactors = FALSE)
+    for(y in seq_len(nrow(grid_others))){ # loop over all combinations of other parameters (e.g. bias, large, he)
+        # filter the data by current parameter combination
+        filters <- lapply(seq_along(grid_others), function(z){
+            lhs <- names(grid_others)[z]
+            op <- quote(`==`)
+            rhs <- grid_others[y, z]
+            expr(`!!`(op)(!!sym(lhs), !!rhs))
+        })
+        img_data <- out_n %>% 
+            filter(!!!filters) %>% 
+            mutate(method = factor(method, 
+                                   levels = c("Harmonic Mean CI (chisq)", "Harmonic Mean Additive CI (chisq)",
+                                              "Harmonic Mean Multiplicative CI (chisq)", "Harmonic Mean CI (f)", 
+                                              "Harmonic Mean Additive CI (f)", "Harmonic Mean Multiplicative CI (f)")))
+        lapply(current_levels, function(z){
+            title <- img_data %>% 
+                filter(!!sym(current) == z) %>% 
+                select(-method, -measure, -value, -all_of(current), -sampleSize) %>%
+                distinct() %>%
+                bind_cols(!!current := z) %>%
+                select(order(colnames(.))) %>%
+                make_title()
+            img_data %>% 
+                filter(!!sym(current) == z) %>% 
+                ggplot(aes(x = measure, y = value)) +
+                geom_col(fill = viridisLite::viridis(1)) +
+                ylim(c(0, 1)) +
+                facet_wrap(~method) +
+                labs(x = "# intervals", y = "relative frequency",
+                     title = eval(title))
+        }) %>% 
+            wrap_plots(guides = "collect") &
+            theme(legend.position = "bottom",
+                  text = element_text(size = 9),
+                  plot.title = element_text(size = 10))
+        ggsave(filename = paste0("figs/rel_freq/", toupper(current), "_", 
+                                 paste0(paste0(names(grid_others[y, ]), "_", grid_others[y, ]), collapse = "_"),
+                                 ".png"),
+               width = length(current_levels) * 7.5,
+               height = 12,
+               units = "in",
+               type = "cairo")
     }
 }
-
-
-for(di in dist){
-    for(la in large){
-        for(bi in bias){
-            for(he in heterogeneity){
-                for(i in I2){
-                    system(paste0("convert \\( ",
-                                  "figs/rel_freq/k_2_",
-                                  di,
-                                  "_bias_", bi,
-                                  "_large_", la,
-                                  "_sim-mod_", he,
-                                  "_I2_", i,
-                                  ".png ",
-                                  "figs/rel_freq/k_3_",
-                                  di,
-                                  "_bias_", bi,
-                                  "_large_", la,
-                                  "_sim-mod_", he,
-                                  "_I2_", i,
-                                  ".png ",
-                                  "+append \\) ",
-                                  "\\( ",
-                                  "figs/rel_freq/k_5_",
-                                  di,
-                                  "_bias_", bi,
-                                  "_large_", la,
-                                  "_sim-mod_", he,
-                                  "_I2_", i,
-                                  ".png ",
-                                  "figs/rel_freq/k_10_",
-                                  di,
-                                  "_bias_", bi,
-                                  "_large_", la,
-                                  "_sim-mod_", he,
-                                  "_I2_", i,
-                                  ".png ",
-                                  "+append \\) ",
-                                  "figs/rel_freq/k_20_",
-                                  di,
-                                  "_bias_", bi,
-                                  "_large_", la,
-                                  "_sim-mod_", he,
-                                  "_I2_", i,
-                                  ".png ",
-                                  "-append figs/rel_freq/STUDIES_", di, "_bias_", bi,"_large_", la, "_sim-mod_", he, "_I2_", i, ".png "))
-                }
-            }
-        }
-    }
-}
-
 
