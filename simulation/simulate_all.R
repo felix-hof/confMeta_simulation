f72ac587a1c56218cb93854265be6eb01c3ef6da
Felix Hofmann <felix.hofmann2@uzh.ch>
2023-07-11 01:00:22 +0200

    Adapt number of cores to olive server

--- simulation/simulate_all.R
+++ simulation/simulate_all.R
@@ -1427,7 +1427,7 @@ grid <- expand.grid(
 
 ## run simulation, e.g., on the Rambo server of I-MATH
 start <- Sys.time()
-out <- sim(grid = grid, N = 5e3, cores = 110)
+out <- sim(grid = grid, N = 5e3, cores = 55)
 # out <- sim(grid = grid[688:703, ], N = 10, cores = 15)
 end <- Sys.time()
 print(end - start)
