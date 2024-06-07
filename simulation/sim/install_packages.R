76243d0d3c219754656ce7ed49422017627d350e
Felix Hofmann <felix_hofmann_13@hotmail.com>
2024-06-07 17:04:31 +0200

    Improve install script

--- simulation/sim/install_packages.R
+++ simulation/sim/install_packages.R
@@ -4,6 +4,17 @@ Rscripts <- paste0(script_dir, "/", list.files(script_dir))
 Rscripts <- Rscripts[grepl(".R$", Rscripts)]
 Rscripts <- Rscripts[!grepl("plot", Rscripts)] # remove plotting
 
+# Function for extracting multiple matches
+extract_matches <- function(data, pattern) {
+    start <-  gregexpr(pattern, data)[[1]]
+    stop  <-  start + attr(start, "match.length") - 1
+    if(-1 %in% start) {
+        ""    ## **Note** you could return NULL if there are no matches 
+    } else {
+        mapply(substr, start, stop, MoreArgs = list(x = data))
+    }
+}
+
 # extract packages used from all R scripts
 libraries <- lapply(Rscripts, function(x){
   # read the scripts
@@ -19,10 +30,17 @@ libraries <- lapply(Rscripts, function(x){
   # get libraries
   libs <- gsub("\\s*library\\((.+)\\)\\s*", "\\1", code[grepl("library\\(", code)])
   reqs <- gsub("\\s*require\\((.+)\\)\\s*", "\\1", code[grepl("require\\(", code)])
+  # get all libraries
   libs <- c(libs, reqs)
   # get this into nice format
   libs <- unlist(strsplit(libs, ","), recursive = TRUE)
   libs <- gsub("\\W", "", libs)
+  # Get namespaced calls
+  ns_calls <- grep(":{2,3}", code, value = TRUE)
+  ns <- lapply(ns_calls, extract_matches, pattern = "([[:alnum:]]+?):{2,3}")
+  ns <- unique(sub(":{2,3}", "", do.call("c", ns)))
+  # return all libraries
+  unique(c(libs, ns))
 })
 
 # unlist
@@ -44,17 +62,16 @@ installed <- vapply(libraries, function(x){
 }, logical(1L), USE.NAMES = TRUE)
 
 # install uninstalled ones CRAN packages
-libs <- libraries[!installed]
-print(libs)
-if(length(libs) == 0){
+install_libs <- libraries[!installed]
+if(length(install_libs) == 0){
 	cat("All necessary libraries already installed.\n")
 } else {
-	cat("Installing packages:\n", paste(libs, collapse = "\n", "\n"))
+	cat("Installing packages:\n", paste(install_libs, collapse = "\n", "\n"))
 	lapply(libs, function(x){
 	  out <- tryCatch({install.packages(x); 0L},
-	    warning = function(w) "NA on CRAN",
-	    error = function(e) "NA on CRAN")
-	  if(out == "NA on CRAN") remotes::install_github(paste0("felix-hof/", x))
+	    warning = function(w) 1L,
+	    error = function(e) 2L)
+	  if(out %in% 2L) remotes::install_github(paste0("felix-hof/", x))
 	  invisible(NULL)
 	})
 }
